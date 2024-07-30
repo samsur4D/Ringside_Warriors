5356fd53b86f490cfb04e7d4ba297db99f9712d7
samsur4D <patarehan14600@gmail.com>
2024-07-30 06:18:31 +0600

    banner responsive donee

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -2,8 +2,8 @@ import banner from "../assets/cover.webp";
 import { PiBoxingGloveFill } from "react-icons/pi";
 import { MdTimer } from "react-icons/md";
 import { GiShardSword } from "react-icons/gi";
-import two from '../assets/twoboxer.jpg'
-import waist from '../assets/waist.jpg'
+import two from "../assets/twoboxer.jpg";
+import waist from "../assets/waist.jpg";
 
 const Banner = () => {
   return (
@@ -11,45 +11,46 @@ const Banner = () => {
       <div className="border-t-2 ">
         <img className="w-full " src={banner} alt="" />
       </div>
-      <div className="flex container mx-auto bg-black  py-20 px-20 ">
-        <div className=" mr-4 ml-4 px-5">
+      {/* _------------------------------------------------------------------------------ */}
+      <div className="flex flex-col md:flex-row lg:flex-row   container mx-auto bg-black  lg:py-20 lg:px-20 ">
+        <div className="md:ml-2 md:mr-2 md:mt-5 lg:mr-4 lg:ml-4 lg:px-5">
           <div className="flex gap-4">
-            <PiBoxingGloveFill className="text-7xl text-[#d34940]" />
-            <h2 className="text-7xl font-semibold font-samu text-[#d34940]">
+            <PiBoxingGloveFill className="text-5xl md:text-4xl lg:text-7xl text-[#d34940]" />
+            <h2 className="text-5xl md:text-3xl lg:text-7xl font-semibold font-samu text-[#d34940]">
               Boxing Academy
             </h2>
           </div>
-          <p className="text-3xl text-white">
+          <p className="text-xl md:text-xl  lg:text-3xl text-white leading-tight">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eius
             dolorem dicta. Beatae, ex. Deserunt, dolores. Ipsum, sequi quae?
           </p>
         </div>
-        <div className="border-x-2 mr-4 ml-4 px-5 ">
+        <div className="border-x-2 md:ml-2 pl-3 md:mr-2 md:mt-5 lg:mr-4 lg:ml-4 lg:px-5">
           <div className="flex gap-4 container mx-auto">
-            <MdTimer className="text-7xl text-[#d34940]" />
-            <h2 className="text-7xl font-semibold font-samu text-[#d34940]">
-              Boxing Academy
+            <MdTimer className="text-5xl md:text-4xl lg:text-7xl text-[#d34940]" />
+            <h2 className="text-5xl md:text-3xl lg:text-7xl font-semibold font-samu text-[#d34940]">
+           
+KICKBOXING Fight
             </h2>
           </div>
-          <p className="text-3xl text-white">
+          <p className="text-xl md:text-xl  lg:text-3xl text-white leading-tight">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eius
             dolorem dicta. Beatae, ex. Deserunt, dolores. Ipsum, sequi quae?
           </p>
         </div>
-        <div className=" mr-4 ml-4 px-5">
+        <div className="md:ml-2 md:mr-2 md:mt-5 lg:mr-4 lg:ml-4 lg:px-5">
           <div className="flex gap-4 items-center ">
-            <GiShardSword className="text-7xl text-[#d34940]" />
-            <h2 className="text-7xl font-semibold font-samu text-[#d34940]">
-              Boxing Academy
+            <GiShardSword className="text-5xl md:text-4xl lg:text-7xl text-[#d34940]" />
+            <h2 className="text-5xl md:text-3xl lg:text-7xl font-semibold font-samu text-[#d34940]">
+            PERSONAL TRAINING
             </h2>
           </div>
-          <p className="text-3xl text-white">
+          <p className="text-xl md:text-xl  lg:text-3xl text-white leading-tight">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt eius
             dolorem dicta. Beatae, ex. Deserunt, dolores. Ipsum, sequi quae?
           </p>
         </div>
       </div>
- 
     </>
   );
 };
--- src/Home/Navber.jsx
+++ src/Home/Navber.jsx
@@ -1,39 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logoone.png";
-import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
 import { IoLogoYoutube } from "react-icons/io";
 
 const Navber = () => {
   const navItems = (
-    <>  
-      <Link className="font-samu px-2 lg:px-10 border border-white/40 py-2 lg:py-4 text-sm lg:text-lg text-white active:text-[#d34940]">Home</Link>
-      <Link className="font-samu px-2 lg:px-10 border border-white/40 py-2 lg:py-4 text-sm lg:text-lg text-white">About</Link>
-      <Link className="font-samu px-2 lg:px-10 border border-white/40 py-2 lg:py-4 text-sm lg:text-lg text-white">Pages</Link>
-      <Link className="font-samu px-2 lg:px-10 border border-white/40 py-2 lg:py-4 text-sm lg:text-lg text-white">Classes</Link>
-      <Link className="font-samu px-2 lg:px-10 border border-white/40 py-2 lg:py-4 text-sm lg:text-lg text-white">Contact</Link>
+    <>
+      <Link className=" font-samu lg:px-10 lg:border border-white/40 py-4   lg:text-lg relative active text-[#d34940]">
+        Home
+      </Link>
+      <Link className=" font-samu lg:px-10 lg:border border-white/40 py-4 relative   lg:text-lg text-white">
+        About
+      </Link>
+      <Link className=" font-samu lg:px-10 lg:border border-white/40 py-4   lg:text-lg  text-white">
+        Pages
+      </Link>
+      <Link className=" font-samu lg:px-10 lg:border border-white/40 py-4   lg:text-lg  text-white">
+        Classes
+      </Link>
+      <Link className=" font-samu lg:px-10 lg:border border-white/40 py-4   lg:text-lg  text-white">
+        Contact
+      </Link>
     </>
   );
 
   return (
-    <section className="bg-black container mx-auto flex flex-wrap items-center justify-between p-3 lg:gap-60">
-      <div className="flex items-center">
-        <img src={logo} alt="Logo" className="w-20 lg:w-28 md:w-24 p-2" />
-      </div>
-      <div className="flex-grow hidden lg:flex justify-center">
+    <>
+      <section className="bg-black container mx-auto flex items-center justify-between  lg:gap-60">
+        <div>
+          <img src={logo} alt="" className="w-32 md:w-32 lg:w-28 p-3" />
+        </div>
+        <div className="hidden lg:block ">{navItems}</div>
+        <div className="flex items-center bg-[#d34940] px-6 lg:px-12 py-4 rounded-xl lg:rounded-none lg:py-10  text-white font-bold gap-12">
+          <button className="bg-[#d34940]  border-r-2 px-7">Login </button>
+          <button className="bg-[#d34940] ">Sign In</button>
+        </div>
+      </section>
+      <div className="mb-6 text-center block lg:hidden space-x-7 md:space-x-16 lg:space-x-0 mt-4">
         {navItems}
       </div>
-      <div className="flex items-center bg-[#d34940] px-4 py-2 lg:px-12 lg:py-10 text-white font-bold gap-4 lg:gap-12">
-        <button className="bg-[#d34940] border-r-2 px-4 lg:px-7">Login</button>
-        <button className="bg-[#d34940] px-4 lg:px-7">Sign In</button>
-      </div>
-      <div className="lg:hidden flex justify-center w-full mt-3">
-        <div className="flex justify-center space-x-8 md:space-x-16">
-          {navItems}
-        </div>
-      </div>
-    </section>
+    </>
   );
 };
 
